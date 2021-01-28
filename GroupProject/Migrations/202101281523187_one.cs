fb57e9a35111eeb13b1b11e456a9751f35fbd19c
Paul Chronakis <paul.chronakis@Peoplecert.onmicrosoft.com>
2021-01-28 22:30:02 +0200

    last

---
+++ GroupProject/Migrations/202101281523187_one.cs
@@ -0,0 +1,22 @@
+﻿namespace GroupProject.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class one : DbMigration
+    {
+        public override void Up()
+        {
+            DropForeignKey("dbo.Orders", "ApplicationUser_Id", "dbo.AspNetUsers");
+            DropIndex("dbo.Orders", new[] { "ApplicationUser_Id" });
+            DropColumn("dbo.Orders", "ApplicationUser_Id");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Orders", "ApplicationUser_Id", c => c.String(maxLength: 128));
+            CreateIndex("dbo.Orders", "ApplicationUser_Id");
+            AddForeignKey("dbo.Orders", "ApplicationUser_Id", "dbo.AspNetUsers", "Id");
+        }
+    }
+}
